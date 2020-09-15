938dfa6c5b4e70c2d3930622ad3ebee9232e95f7
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2020-09-15 14:05:09 +0930

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
+using System;
+
+namespace Authentication_Logic_Concept____Innovation_Challenge_
+{
+    class Program
+    {
+        // Global Variables:
+        string business_key = "-";
+        string access_point_name = "Name";
+        // Code made up of serveral elements. (Date and time).
+        string store_item_code = "TEST";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Step 1:");
+        }
+    }
+}
 
