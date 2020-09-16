3d31ef2453c46a48cd3377c6088be4709434c1c1
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2020-09-16 18:02:23 +0930

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,14 +6,38 @@ namespace Authentication_Logic_Concept____Innovation_Challenge_
     {
         // Global Variables:
         string business_key = "-";
-        string access_point_name = "Name";
-        // Code made up of serveral elements. (Date and time).
-        string store_item_code = "TEST";
+        string access_point_name = "Business_Name";
+        // Code made up of several elements. (Date and time).
+        static string store_item_code = "TEST2020-09-16";
 
         static void Main(string[] args)
         {
+            Program main_program = new Program();
             Console.WriteLine("Step 1:");
+            main_program.check_expiry(store_item_code);
+        }
+        public void check_expiry(string store_item_code)
+        { 
+            // Reads QR code or barcode to extract time/date and compares with current time/date to determine validity.
+            Console.WriteLine("Check Expiry");
+            string current_time = DateTime.Now.ToString("yyyy-MM-dd h:mm:ss tt");
+            Console.WriteLine(DateTime.Now.ToString("yyyy"));
+            string year = 
+
+            // Check for illegitimate time/dates.
+            if (DateTime.Now.ToString("yyyy") <= )
+            {
+
+            }
+
+        }
+        public void initial_verification(string current_path)
+        {
+            Console.WriteLine("Initial Verification");
+        }
+        public static string ---(bool ---)
+        {
+            return "!";
         }
     }
 }
-
