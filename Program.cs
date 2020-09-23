e8c95c5a1bbf4884024d9bf75467a53f679f15f0
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2020-09-23 13:45:59 +0930

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,55 +10,71 @@ namespace Authentication_Logic_Concept____Innovation_Challenge_
         string business_key = "pass";
         // Extract access point names.
         static string access_point_name = "Business_Name";
-        // Code made up of several elements. (Date/time & access point name).
+        // Code made up of several elements. (Date/time + access point name + part of password).
+        // Ideally scrambled to avoid illegitimate modifications.
         static string store_item_code = DateTime.Now.ToString("yyyy-MM-dd h:mm:ss tt") + "|" + access_point_name + "|" + "word";
+
+        static bool verified = true;
 
         static void Main(string[] args)
         {
             Program main_program = new Program();
 
             // Check whether the customer has purchased an item within 30mins.
+            Console.WriteLine("Check Expiry");
             main_program.check_expiry(store_item_code);
 
             // Verify that the item the customer purchased is from the same store providing wifi.
+            Console.WriteLine("Initial Verification");
             main_program.initial_verification(store_item_code);
-
-            main_program.extract_access_point_password(store_item_code);
-
+            string final = "";
+            if (verified == true)
+            {
+                // Once all steps have been verified, access point password is extracted.
+                Console.WriteLine("Extract access point password");
+                final = main_program.extract_access_point_password(store_item_code);
+                Console.WriteLine(final);
+            }
+            else
+            {
+                System.Environment.Exit(0);
+            }
+            // Use newly acquired password to access store's access point without the need for the user to enter manually.
+            main_program.connect_to_access_point(final);
         }
         public void check_expiry(string store_item_code)
         {
-            Console.WriteLine("Check Expiry");
             if ((DateTime.Now - DateTime.Parse(store_item_code.Remove(store_item_code.IndexOf('|')))).TotalMinutes <= 30)
             {
-                Console.WriteLine("Valid");
+                Console.WriteLine("Valid Expiry");
             }
             else
             {
                 Console.WriteLine("Invalid Code");
-                System.Environment.Exit(0);
+                verified = false;
             }
         }
         public void initial_verification(string store_item_code)
         {
-            Console.WriteLine("Initial Verification");
-            Console.WriteLine(access_point_name);
-            Console.WriteLine(store_item_code.Substring(store_item_code.IndexOf('|') + 1, store_item_code.LastIndexOf('|') - store_item_code.IndexOf('|') + 1));
-            if (access_point_name == store_item_code.Substring(store_item_code.IndexOf('|') + 1, store_item_code.LastIndexOf('|') - store_item_code.IndexOf('|') + 1))
+            Console.WriteLine(access_point_name + " = " + store_item_code.Substring(store_item_code.IndexOf('|') + 1, store_item_code.LastIndexOf('|') - store_item_code.IndexOf('|') - 1));
+            if (access_point_name == store_item_code.Substring(store_item_code.IndexOf('|') + 1, store_item_code.LastIndexOf('|') - store_item_code.IndexOf('|') - 1))
             {
-                Console.WriteLine("Valid");
+                Console.WriteLine("Valid Code");
             }
             else
             {
                 Console.WriteLine("Invalid Code");
-                System.Environment.Exit(0);
+                verified = false;
             }
         }
-        public void extract_access_point_password(string store_item_code)
+        public string extract_access_point_password(string store_item_code)
         {
-            Console.WriteLine("Extract access point password");
             string password = (business_key + store_item_code.Substring(store_item_code.LastIndexOf('|') + 1));
-            Console.WriteLine(password);
+            return ("Wifi password: " + password);
+        }
+        public void connect_to_access_point(string password)
+        {
+
         }
     }
 }
